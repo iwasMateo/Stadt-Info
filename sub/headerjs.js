15d4cbf99c4650b5b90959688c24b2540147098a
iwasMateo <iwasMateo@gmail.com>
2026-02-17 11:50:30 +0000

    Kontakt Grundlagen

---
+++ sub/headerjs.js
@@ -0,0 +1,12 @@
+const header = document.getElementById("headerid");
+
+header.innerHTML = `
+    <div id="main-logo">
+    <img src="logo.webp">
+    <h1><a href="../index.html">Brolen</a></h1>
+    </div>
+    <h1><a href="forum.html">Forum</a></h1>
+    <h1><a href="attraktion.html">Attraktionen</a></h1>
+    <h1><a href="kontakt.html">Kontakt</a></h1>
+    <link rel="stylesheet" href="headerstyle.css">
+`;
