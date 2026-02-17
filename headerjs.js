83621eef23807738dba1a13f701cf14dd3bde64d
iwasMateo <iwasmateo@gmail.com>
2026-02-17 17:15:31 +0100

    2. Skript wieder gefunden

--- headerjs.js
+++ headerjs.js
@@ -3,7 +3,7 @@ const header = document.getElementById("headerid");
 header.innerHTML = `
     <div id="main-logo">
     <img src="logo.webp">
-    <h1><a href="index.html">Borlean</a></h1>
+    <h1><a href="/index.html">Borlean</a></h1>
     </div>
     <h1><a href="sub/forum.html">Forum</a></h1>
     <h1><a href="sub/attraktion.html">Attraktionen</a></h1>
--- sub/headerjs.js
+++ sub/headerjs.js
@@ -3,7 +3,7 @@ const header = document.getElementById("headerid");
 header.innerHTML = `
     <div id="main-logo">
     <img src="logo.webp">
-    <h1><a href="../index.html">Brolen</a></h1>
+    <h1><a href="../index.html">Borlean</a></h1>
     </div>
     <h1><a href="forum.html">Forum</a></h1>
     <h1><a href="attraktion.html">Attraktionen</a></h1>
