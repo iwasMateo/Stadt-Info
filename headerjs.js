90e47c3506f3f0f6af4639b39e875c295cd0a906
iwasMateo <iwasmateo@gmail.com>
2026-02-17 14:45:03 +0100

    Fix für den header

--- headerjs.js
+++ headerjs.js
@@ -5,8 +5,8 @@ header.innerHTML = `
     <img src="logo.webp">
     <h1><a href="index.html">Brolen</a></h1>
     </div>
-    <h1><a href="forum.html">Forum</a></h1>
-    <h1><a href="attraktion.html">Attraktionen</a></h1>
-    <h1><a href="kontakt.html">Kontakt</a></h1>
+    <h1><a href="sub/forum.html">Forum</a></h1>
+    <h1><a href="sub/attraktion.html">Attraktionen</a></h1>
+    <h1><a href="sub/kontakt.html">Kontakt</a></h1>
     <link rel="stylesheet" href="headerstyle.css">
 `;
