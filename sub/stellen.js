7994b603b34dfda1be307f51c3c298de1b8e7c6e
iwasMateo <iwasMateo@gmail.com>
2026-03-06 11:14:43 +0000

    sachen ka

--- sub/stellen.js
+++ sub/stellen.js
@@ -1,26 +1,40 @@
 let opLohn = ["22€", "25€", "27€"];
+let opStunden = ["35h", "40h", "45h"];
+let alleStunden = [
+    [["22€", "25€", "27€"], ["35h", "40h", "45h"]], 
+    [["22€", "25€", "27€"], ["35h", "40h", "45h"]]
+];
 let lohnElement = document.getElementById("Stundenlohn"); //finde den Tag
-let lohn = lohnElement.innerText; // der text im element // Löhne, die man haben kann
 function changeStunden(type) {
+    let lohn = lohnElement.innerText; // der text im element // Löhne, die man haben kann
+    let stunden = document.getElementById("Stunden"); // Aktuelle Studnen
     if (opLohn.includes(lohn)) { // falls der lohn in den optionen für lohn ist, dann:
         if (type === "add") { // falls der angegebene typ "add" ist, dann:
             let index = opLohn.indexOf(lohn);
+            console.log(opLohn[index]);
+            console.log(index);
             if(index < 2) { // checken ob index kleiner als 2 ist damit es nicht für index 3 guckt
                 index +=1;
-                lohnElement.innerText = opLohn[index];
+                console.log(index);
+                stunden.innerText = opStunden[index]; // Text für die Stunden setzen
+                lohnElement.innerText = opLohn[index]; // text für den Lohn setzen
             } else {
                 console.log("nix");
             }     
         } else if (type === "subtract") { // gleiche sache wie davor aber
             let index = opLohn.indexOf(lohn);
+            console.log(index);
             if(index > 0) { // wenn der lohn größer als 0 ist (listen fangen bei 0 an)
                 index -=1;
-                lohnElement.innerText = opLohn[index];
+                console.log(index);
+                stunden.innerText = opStunden[index]; // Text für die Stunden setzen
+                lohnElement.innerText = opLohn[index]; // Text für  den Lohn setzen
             } else {
                 console.log("nix");
             }
         }
-    }else { // falls es nd da drin ist, dann 
+    }else { // falls es nd da drin ist, dann
+        console.log("What"); 
         index = 1;
         lohnElement.innerText = opLohn[1]; // setzt es zu opLohn[1](der standard)
     }
