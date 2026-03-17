470c658c837c55805cdbd7e6342b443b6b60751b
iwasMateo <iwasMateo@gmail.com>
2026-03-17 11:21:18 +0000

    Start mit fixen, möchte grad nur pull machen

--- sub/stellen.js
+++ sub/stellen.js
@@ -1,34 +1,37 @@
 let opLohn = ["22€", "25€", "27€"];
 let opStunden = ["35h", "40h", "45h"];
 let alleStunden = [
-    [["22€", "25€", "27€"], ["35h", "40h", "45h"]], 
+    [, ["35h", "40h", "45h"]], 
     [["22€", "25€", "27€"], ["35h", "40h", "45h"]]
 ];
-let lohnElement = document.getElementById("Stundenlohn"); //finde den Tag
-function changeStunden(type) {
+
+function changeStunden(type, id, wagearray) {
+    let fulldiv = document.getElementById(id);
+    let maindiv = fulldiv.querySelector(".lohndiv");
+    let lohnElement = fulldiv.querySelector(".Stundenlohn"); //finde den Tag
     let lohn = lohnElement.innerText; // der text im element // Löhne, die man haben kann
-    let stunden = document.getElementById("Stunden"); // Aktuelle Studnen
+    let stunden = maindiv.querySelector(".Stunden"); // Aktuelle Studnen
     if (opLohn.includes(lohn)) { // falls der lohn in den optionen für lohn ist, dann:
         if (type === "add") { // falls der angegebene typ "add" ist, dann:
-            let index = opLohn.indexOf(lohn);
-            console.log(opLohn[index]);
+            let index = wagearray.indexOf(lohn);
+            console.log(wagearray[index]);
             console.log(index);
             if(index < 2) { // checken ob index kleiner als 2 ist damit es nicht für index 3 guckt
                 index +=1;
                 console.log(index);
                 stunden.innerText = opStunden[index]; // Text für die Stunden setzen
-                lohnElement.innerText = opLohn[index]; // text für den Lohn setzen
+                lohnElement.innerText = wagearray[index]; // text für den Lohn setzen
             } else {
                 console.log("nix");
             }     
         } else if (type === "subtract") { // gleiche sache wie davor aber
-            let index = opLohn.indexOf(lohn);
+            let index = wagearray.indexOf(lohn);
             console.log(index);
             if(index > 0) { // wenn der lohn größer als 0 ist (listen fangen bei 0 an)
                 index -=1;
                 console.log(index);
                 stunden.innerText = opStunden[index]; // Text für die Stunden setzen
-                lohnElement.innerText = opLohn[index]; // Text für  den Lohn setzen
+                lohnElement.innerText = wagearray[index]; // Text für  den Lohn setzen
             } else {
                 console.log("nix");
             }
